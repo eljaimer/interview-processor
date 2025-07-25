e896855403f5e315cd261834f88508c0ae776fcf
eljaimer <110939489+eljaimer@users.noreply.github.com>
2025-07-25 13:07:52 -0700

    Create card.js

---
+++ src/components/ui/card.js
@@ -0,0 +1,21 @@
+```javascript
+import React from 'react';
+
+export const Card = ({ children, className = '' }) => (
+  
+    {children}
+  
+);
+
+export const CardHeader = ({ children, className = '' }) => (
+  {children}
+);
+
+export const CardTitle = ({ children, className = '' }) => (
+  {children}
+);
+
+export const CardContent = ({ children, className = '' }) => (
+  {children}
+);
+```
